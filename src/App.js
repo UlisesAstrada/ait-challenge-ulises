178ae30ad44d97776d60b276f6c261cc2103140c
Ulises <ulises_astrada@outlook.com>
2021-12-10 10:35:36 -0300

    Adding features

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
+import { Button } from 'react-bootstrap';
+
 import './App.css';
 
 function App() {
 
   const [products, setProducts] = useState([])
 
+  const [cart, setCart] = useState([])
+
+  const [stock, setStock] = useState()
+
+
   useEffect(() =>
   fetch('https://ait-tesapi.herokuapp.com/products')
     .then(response => response.json())
     .then((res) => setProducts(res.products))
+    .catch(err => new Error(err))
   ,[])
 
   return (
@@ -17,14 +25,40 @@ function App() {
       <header><h1>Productos</h1></header>
       <div className='container'>
       {products.map((item) => 
-        <ul>
+        <ul key={item._id}  >
           <li className='bg-primary'>
             <h4>{item.name}</h4>
             <img src={item.image} alt={item.name}></img>
+            <b>Stock: {item.stock} unidades</b>
+            <h5>Precio: {item.price}</h5>
+            {item.stock === 0 ? 
+              <button 
+                type='button' 
+                className='btn btn-success' 
+                disabled
+              >
+                Añadir
+              </button>
+              :
+              <button
+                type='button' 
+                className='btn btn-success'
+              >
+                Añadir
+              </button>
+            }
           </li>
         </ul>
       )}
       </div>
+      <footer className='d-flex bg-primary justify-content-center py-3'>
+        <Button 
+          variant="success"
+        >
+          Ver carrito
+        </Button>
+
+      </footer>
     </div>
   );
 }
